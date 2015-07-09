0c1dde47d040e5a58ca53f12351670b6d014f530
Li Zhijian <lizhijian@cn.fujitsu.com>
2015-07-09 13:38:41 +0800

    fix to colo-v1.4-basic
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>

--- secondary-colo.sh
+++ secondary-colo.sh
@@ -13,9 +13,9 @@ forward_dev=eth1
 
 tmp_disk_size=`./qemu-img info $disk_path |grep 'virtual size' |awk  '{print $3}'`
 
-net_param="-netdev tap,id=hn0,script=/etc/qemu-ifup,downscript=/etc/qemu-ifdown,colo_script=./scripts/colo-proxy-script.sh,colo_nicname=${forward_dev},vhost=on -device virtio-net-pci,id=net-pci0,netdev=hn0"
+net_param="-netdev tap,id=hn0,script=/etc/qemu-ifup,downscript=/etc/qemu-ifdown,colo_script=./scripts/colo-proxy-script.sh,forward_nic=${forward_dev},vhost=on -device virtio-net-pci,id=net-pci0,netdev=hn0"
 
-block_param="-drive if=none,file=$disk_path,id=nbd_target1 -drive if=virtio,driver=replication,mode=secondary,file.file.filename=$active_disk,file.driver=qcow2,export=colo1,file.backing_reference.drive_id=nbd_target1,file.backing_reference.hidden-disk.file.filename=$hidden_disk,file.backing_reference.hidden-disk.driver=qcow2,file.backing_reference.hidden-disk.allow-write-backing-file=on"
+block_param="-drive if=none,file=$disk_path,id=colo1 -drive if=virtio,driver=replication,mode=secondary,throttling.bps-total-max=70000000,file.file.filename=$active_disk,file.driver=qcow2,file.backing.file.filename=$hidden_disk,file.backing.driver=qcow2,file.backing.allow-write-backing-file=on,file.backing.backing.backing_reference=colo1"
 
 cmdline="x86_64-softmmu/qemu-system-x86_64 -enable-kvm $net_param -boot c $block_param -vnc :7 -m 2048 -smp 2 -device piix3-usb-uhci -device usb-tablet -monitor stdio -incoming tcp:0:8888"
 
@@ -42,6 +42,7 @@ prepare_temp_images
 echo $cmdline
 echo
 echo "Please Enter: nbd_server_start $nbdserver"
+echo "Please Enter: nbd_server_add -w colo1"
 echo
 modprobe xt_SECCOLO
 modprobe nf_conntrack_colo
